61b54e99a8dd7137d9c0dab458f1cd0897a4cc8a
deltadev-rsc <deltaqxq174@mail.ru>
2026-03-13 16:45:48 +0500

    Create fff.lua

---
+++ .config/nvim/lua/plugins/fff.lua
@@ -0,0 +1,115 @@
+require('fff').setup({
+    base_path = vim.fn.getcwd(),
+    prompt = ' 󰍉 : ',
+    title = 'Find Files',
+    max_results = 100,
+    max_threads = 3,
+    lazy_sync = true, 
+    layout = {
+        height = 0.8,
+        width = 0.8,
+        prompt_position = 'top', 
+        preview_position = 'right', 
+        preview_size = 0.6,
+        show_scrollbar = true,
+        flex = {
+            size = 130,
+            wrap = 'top',
+        },
+        show_scrollbar = true,
+    },
+
+    preview = {
+        enabled = true,
+        max_size = 10 * 1024 * 1024, 
+        chunk_size = 8192, 
+        binary_file_threshold = 1024, 
+        imagemagick_info_format_str = '%m: %wx%h, %[colorspace], %q-bit',
+        line_numbers = false,
+        wrap_lines = false,
+        filetypes = {
+            svg = { wrap_lines = true },
+            markdown = { wrap_lines = true },
+            text = { wrap_lines = true },
+        },
+    },
+    
+    keymaps = {
+        close = '<Esc>',
+        select = '<CR>',
+        select_split = '<C-s>',
+        select_vsplit = '<C-v>',
+        select_tab = '<C-t>',
+        move_up = { '<Up>', '<C-p>' },
+        move_down = { '<Down>', '<C-n>' },
+        preview_scroll_up = '<C-u>',
+        preview_scroll_down = '<C-d>',
+        toggle_debug = '<F2>',
+        cycle_previous_query = '<C-Up>',
+        toggle_select = '<Tab>',
+        send_to_quickfix = '<C-q>',
+    },
+
+    hl = {
+        border = 'FloatBorder',
+        normal = 'Normal',
+        cursor = 'CursorLine',
+        matched = 'IncSearch',
+        title = 'Title',
+        prompt = 'Question',
+        active_file = 'Visual',
+        frecency = 'Number',
+        debug = 'Comment',
+        combo_header = 'Number',
+        scrollbar = 'Comment', 
+        directory_path = 'Comment',
+        selected = 'FFFSelected',
+        selected_active = 'FFFSelectedActive',
+        git_staged = 'FFFGitStaged',
+        git_modified = 'FFFGitModified',
+        git_deleted = 'FFFGitDeleted',
+        git_renamed = 'FFFGitRenamed',
+        git_untracked = 'FFFGitUntracked',
+        git_ignored = 'FFFGitIgnored',
+        git_sign_staged = 'FFFGitSignStaged',
+        git_sign_modified = 'FFFGitSignModified',
+        git_sign_deleted = 'FFFGitSignDeleted',
+        git_sign_renamed = 'FFFGitSignRenamed',
+        git_sign_untracked = 'FFFGitSignUntracked',
+        git_sign_ignored = 'FFFGitSignIgnored',
+        git_sign_staged_selected = 'FFFGitSignStagedSelected',
+        git_sign_modified_selected = 'FFFGitSignModifiedSelected',
+        git_sign_deleted_selected = 'FFFGitSignDeletedSelected',
+        git_sign_renamed_selected = 'FFFGitSignRenamedSelected',
+        git_sign_untracked_selected = 'FFFGitSignUntrackedSelected',
+        git_sign_ignored_selected = 'FFFGitSignIgnoredSelected',
+    },
+
+    frecency = {
+        enabled = true,
+        db_path = vim.fn.stdpath('cache') .. '/fff_nvim',
+    },
+
+    history = {
+        enabled = true,
+        db_path = vim.fn.stdpath('data') .. '/fff_queries',
+        min_combo_count = 3, 
+        combo_boost_score_multiplier = 100, 
+    },
+
+    git = {
+        status_text_color = true, 
+    },
+
+    debug = {
+        enabled = false, 
+        show_scores = false,
+        show_file_info = false, 
+    },
+
+    logging = {
+        enabled = false,
+        log_file = vim.fn.stdpath('log') .. '/fff.log',
+        log_level = 'info',
+    }
+})
