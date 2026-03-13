9a79739c66e934e5eea6207e1124527e8e886d4f
deltadev-rsc <deltaqxq174@mail.ru>
2026-03-13 16:48:14 +0500

    Create themery.lua

---
+++ .config/nvim/lua/plugins/themery.lua
@@ -0,0 +1,12 @@
+require("themery").setup({
+    themes = {
+        "catppuccin", -- "catppuccin-mocha", 
+        "gruvbox",
+        "everforest",
+        "nordic",
+        "tokyonight-night",
+        "ayu-mirage",
+        "kanagawa-wave",
+    }, 
+    livePreview = true, 
+})
