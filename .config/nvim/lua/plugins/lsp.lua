dc68a03ecaeb6cc78607fe77de551c69343398e1
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-10 16:22:02 +0500

    Update lsp.lua

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -17,4 +17,46 @@ vim.lsp.config('rust_analyzer', {
 })
 
 
-vim.lsp.config('clangd', {})
+vim.lsp.config('clangd', {
+    cmd = { "clangd" },
+    filetypes = {
+        "c",
+        "cpp",
+        "objc",
+        "objcpp",
+        "cuda"
+    },
+    
+    root_markers = {
+        ".clangd",
+        ".clangd-tidy",
+        ".clangd-format",
+        "compile_commands.json",
+        "compile_flags.txt",
+        "configure.ac",
+        ".git"
+    },
+
+    capabilities = {
+        offsetEncoding = {
+            "utf-8",
+            "utf-16"
+        },
+        textDocument = {
+            completion = {
+                editsNearCursor = true
+            }
+        }
+    }
+})
+
+vim.lsp.config('bash-language-server', {
+    cmd = { 'bash-language-server', 'start' },
+    filetypes = { 'bash', 'sh' },
+    root_dir = function(fname)
+        return vim.fs.dirname(vim.fs.find('.git', {
+            path = fname,
+            upward = true
+        }) [1])
+    end
+})
