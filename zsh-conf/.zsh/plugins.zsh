7b6a9e38ca4bd3e0060f2896388544fd9d4087a8
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-03 21:13:47 +0500

    Create plugins.zsh

---
+++ zsh-conf/.zsh/plugins.zsh
@@ -0,0 +1,7 @@
+plugins=(git)
+
+source ~/zsh-plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+source ~/zsh-plugins/zsh-syntax-highlighting/themes/catppuccin_mocha-zsh-syntax-highlighting.zsh
+source ~/zsh-plugins/catppuccin-powerlevel10k-themes/catppuccin-powerlevel10k-themes.plugin.zsh
+source ~/zsh-plugins/catppuccin-powerlevel10k-themes/themes/.p10k-classic-catppuccin-mocha.zsh
+source $ZSH/oh-my-zsh.sh
