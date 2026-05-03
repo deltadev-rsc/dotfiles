dac0ce71c1c49b2bf03ce9350e1d63663075927c
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-03 21:15:37 +0500

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,4 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
 end
 
 set -g -x fish_greeting ''
@@ -12,10 +11,13 @@ alias nv="nvim"
 alias dnv="doas nvim"
 alias v="vim"
 alias dv="doas vim"
-alias ls="exa --tree --level=1 --icons --color=always --no-time --no-user --no-permissions"
-alias cat="bat --theme=Catppuccin\ Mocha"
+alias ls="exa --level=1 --icons --color=always --no-time --no-user --no-permissions"
+alias cat="bat --theme=base16-256"
 alias sudo="doas"
 alias ff="fastfetch"
+alias lg="lazygit"
+alias yz="yazi"
+alias nf="neofetch"
 
 if command -v tree >/dev/null
     alias tree='tree -C -F --dirsfirst'
@@ -25,35 +27,6 @@ set -U fish_history 50000
 
 set -g fish_complete_path $fish_complete_path /usr/share/fish/completions
 
-function fish_prompt
-    set_color normal
-    set -l reset (set_color normal)
-
-    set -l reset (set_color normal)
-    set -l user_color (set_color -o cyan)
-    set -l host_color (set_color -o green)
-    set -l git_clean_color (set_color -o green)
-    set -l git_dirty_color (set_color -o red)
-    set -l error_color (set_color -o red)
-    set -l info_color (set_color -o yellow)
-
-    set -l symbol_branch ""
-    set -l symbol_modified "✗"
-    set -l symbol_arrow ""
-    set -l symbol_success "✔"
-    set -l symbol_fail "✖"
-
-    set -l whoami (whoami)
-    set -l short_host (uname -n | cut -d. -f1)
-    if test "$whoami" != "$short_host"
-        printf "%s%s@%s%s " $user_color $whoami $host_color $short_host $reset
-    end
-
-    set -l cwd (prompt_pwd)
-    printf "%s%s%s " $path_color $cwd $reset
-
-    printf "  "
-end
-
 zoxide init fish | source
 
+export PATH="$HOME/.fuelup/bin:$PATH"
